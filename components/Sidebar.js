31cf019118bd9c19127ef04adcca837689b7d3a7
unknown <erik17242@gmail.com>
2022-11-01 22:37:19 +0200

    added auth

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -11,8 +11,10 @@ import {
   InboxIcon,
   UserIcon,
 } from "@heroicons/react/outline";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Sidebar() {
+  const { data: session } = useSession();
   return (
     <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24">
       {/* Twitter logo */}
@@ -28,32 +30,46 @@ export default function Sidebar() {
       <div className="mt-4 mb-2.5 xl:items-start">
         <SidebarMenuItem text="Home" Icon={HomeIcon} active />
         <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
-        <SidebarMenuItem text="Notifications" Icon={BellIcon} />
-        <SidebarMenuItem text="Messages" Icon={InboxIcon} />
-        <SidebarMenuItem text="Bookmark" Icon={BookmarkAltIcon} />
-        <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
-        <SidebarMenuItem text="Profile" Icon={UserIcon} />
-        <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} />
+        {session && (
+          <>
+            <SidebarMenuItem text="Notifications" Icon={BellIcon} />
+            <SidebarMenuItem text="Messages" Icon={InboxIcon} />
+            <SidebarMenuItem text="Bookmark" Icon={BookmarkAltIcon} />
+            <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
+            <SidebarMenuItem text="Profile" Icon={UserIcon} />
+            <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} />{" "}
+          </>
+        )}
       </div>
 
       {/* Button */}
-      <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
-        Tweet
-      </button>
+      {session ? (
+        <>
+          <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
+            Tweet
+          </button>
 
-      {/* Miniprofile */}
-      <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
-        <img
-          src="https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/RX4OQ5T6HYI6RJR7PNOSVOT2YU.jpg&w=1200"
-          alt="user profile img"
-          className="h-10 w-10 rounded-full xl:mr-2"
-        />
-        <div className="leading-5 hidden xl:inline">
-          <h4 className="font-bold">Tudorica Eric</h4>
-          <p className="text-gray-500">@TudoricaEric</p>
-        </div>
-        <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
-      </div>
+          <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
+            <img
+              src={session?.user.image}
+              onClick={signOut}
+              className="h-10 w-10 rounded-full xl:mr-2"
+            />
+            <div className="leading-5 hidden xl:inline">
+              <h4 className="font-bold">{session.user.name}</h4>
+              <p className="text-gray-500">@{session.user.username}</p>
+            </div>
+            <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
+          </div>
+        </>
+      ) : (
+        <button
+          onClick={() => signIn()}
+          className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline"
+        >
+          Sign in
+        </button>
+      )}
     </div>
   );
 }
---
+++ pages/auth/signin.js
@@ -0,0 +1,42 @@
+import { getProviders, signIn } from "next-auth/react";
+
+export default function signin({ providers }) {
+  return (
+    <div className="flex justify-center mt-20 space-x-4">
+      <img
+        src="https://cdn.cms-twdigitalassets.com/content/dam/blog-twitter/official/en_au/company/2018/lifeline/Lifelin1.png.img.fullhd.medium.png"
+        alt="twitter img"
+        className="object-cover md:w-44 md:h-80 rotate-6 hidden md:inline-flex"
+      />
+      <div className="">
+        {Object.values(providers).map((provider) => (
+          <div className="flex flex-col items-center">
+            <img
+              className="w-36 object-cover "
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/2491px-Twitter-logo.svg.png"
+              alt="twitter logo"
+            />
+            <p className="text-center text-sm italic my-10 ">
+              This app is created for learning purposes
+            </p>
+            <button
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+              className="bg-red-400 rounded-lg p-3 text-white hover:bg-red-500"
+            >
+              Sign in with {provider.name}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: {
+      providers,
+    },
+  };
+}
