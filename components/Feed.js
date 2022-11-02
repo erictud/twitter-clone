e19ce9a7433660b9502bcaead0508efa22908a32
unknown <erik17242@gmail.com>
2022-11-02 22:16:50 +0200

    Added like and delete functionality

--- components/Feed.js
+++ components/Feed.js
@@ -1,30 +1,20 @@
 import { SparklesIcon } from "@heroicons/react/outline";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
 import Input from "./Input";
 import Post from "./Post";
 
 export default function Feed() {
-  const posts = [
-    {
-      id: "1",
-      name: "Eric",
-      username: "codewitheric",
-      userImg:
-        "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/RX4OQ5T6HYI6RJR7PNOSVOT2YU.jpg&w=1200",
-      img: "https://images.unsplash.com/photo-1589656966895-2f33e7653819?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-      text: "cute bear",
-      timestamp: "2 hours ago",
-    },
-    {
-      id: "2",
-      name: "Eric",
-      username: "codewitheric",
-      userImg:
-        "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/RX4OQ5T6HYI6RJR7PNOSVOT2YU.jpg&w=1200",
-      img: "https://images.unsplash.com/photo-1591824438708-ce405f36ba3d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      text: "awww",
-      tiemstamp: "2 minutes ago",
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+  useEffect(
+    () =>
+      onSnapshot(query(collection(db, "posts"), orderBy("timestamp", "desc")), (snapshot) => {
+        setPosts(snapshot.docs);
+      }),
+    []
+  );
+
   return (
     <div className="xl:ml-[370px] border-l xl:min-w-[576px] border-r border-gray-200 sm:ml-[73px] flex-grow max-w-xl">
       <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b ">
